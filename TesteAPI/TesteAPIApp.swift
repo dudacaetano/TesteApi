851a1f0d90fc5e20c41a88b4ac4fcff1eafd9abc
duda caetano <mariaeduardascaetano@gmail.com>
2024-08-22 10:27:43 -0300

    Initial Commit

---
+++ TesteAPI/TesteAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteAPIApp.swift
+//  TesteAPI
+//
+//  Created by Maria Eduarda on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TesteAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
