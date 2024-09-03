b13b936759e69709a8e0aea9cbbfd188ae5044f1
Cintia Braulino <cintiabraulino@gmail.com>
2024-09-03 09:42:46 -0300

    add homeView

--- TesteAPI/View/HomeView.swift
+++ TesteAPI/View/HomeView.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationView {
-
-        }
+        TabView{
+                    
+                    PostView()
+                         .tabItem {
+                            Image(systemName: "line.3.horizontal.circle.fill")
+                            Text("Feed")
+                        }
+                    UploadPostView()
+                        .tabItem {
+                            Image(systemName: "plus.circle")
+                            Text("Publicar")
+                        }
+                    TestView2()
+                        .tabItem {
+                            Image(systemName: "cart.fill")
+                            Text("Shop")
+                        }
+                    PerfilView()
+                        .tabItem {
+                            Image(systemName: "person.crop.circle")
+                            Text("Perfil")
+                        }
+                    
+                }
+                .accentColor(.red)
        
     }
 }
